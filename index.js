be8a91ac544b7f60d95b8e8c45be4bd195c9cd7c
Adablui <89608525+Adablui@users.noreply.github.com>
2024-03-15 18:18:43 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+//السورس الاساسي لـ اظهار رابط موقعك في ريبل ات وجعل البوت الخاص فيك 24ساعة
+const express = require("express")
+const app = express();
+
+app.listen(() => console.log("I'm Ready To Work..! 24H"));
+app.get('/', (req, res) => {
+  res.send(`
+  <body>
+  <center><h1>Bot 24H ON!</h1></center
+  </body>`)
+});
+//https://ra3dstudio.com CopyRight Codes
+//كود اوتو رياكشن على اي بوت او شخص تحدد انت | Autoreaction to any bot or user
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Client(); // All partials are loaded automatically
+
+client.on('ready', async () => {
+  console.log(`${client.user.username} is ready!`);
+})
+//ثبات فويس 24 ساعه v13 بدون اي مشاكل
+const { joinVoiceChannel } = require('@discordjs/voice');
+client.on('ready', () => {
+
+    setInterval( async () => {
+    client.channels.fetch(process.env.channel) 
+     .then((channel) => { 
+      const VoiceConnection = joinVoiceChannel({
+       channelId: channel.id, 
+       guildId: channel.guild.id, 
+       adapterCreator: channel.guild.voiceAdapterCreator 
+       });
+    }).catch((error) => { return; });
+    }, 1000)
+}); 
+//https://ra3dstudio.com CopyRight Codes
+client.login(process.env.token);
+//https://ra3dstudio.com CopyRight Codes
